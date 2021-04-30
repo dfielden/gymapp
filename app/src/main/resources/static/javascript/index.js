9862d8b770059f790a5bfea37e861a1c220c365c
Dan Fielden <dan@danfielden.com>
2021-04-30 18:04:39 +0100

    created slide-up forms to add/edit exercises

--- app/src/main/resources/static/javascript/index.js
+++ app/src/main/resources/static/javascript/index.js
@@ -14,7 +14,9 @@ function getAllExercises() {
 //// SLIDING BUTTONS ////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let slidingDivs = document.querySelectorAll(".slider");
+const slidingDivs = document.querySelectorAll(".slider");
+const slidingBtnContainer = document.querySelectorAll(".slide-on-btn-container");
+
 for (let i = 0; i < slidingDivs.length; i++) {
     slidingDivs[i].addEventListener("touchstart", startTouch, false);
     slidingDivs[i].addEventListener("touchmove", moveTouch, false);
@@ -38,24 +40,24 @@ function moveTouch(e) {
         return;
     }
 
-    let currentX = e.touches[0].clientX;
-    let currentY = e.touches[0].clientY;
-    let diffX = initialX - currentX;
-    let diffY = initialY - currentY;
+    const currentX = e.touches[0].clientX;
+    const currentY = e.touches[0].clientY;
+    const diffX = initialX - currentX;
+    const diffY = initialY - currentY;
 
+    // sliding horizontally
     if (Math.abs(diffX) > Math.abs(diffY)) {
-        // sliding horizontally
+        // swiped left
         if (diffX > 0) {
-            e.preventDefault(); // prevent sideways scrolling
-            // swiped left
-            console.log("swiped left");
-            e.target.closest('.slider').querySelector('.slide-on-btn-container').style.transform = `translateX(calc(100vw - 18.5rem))`; // ensure we are targeting the slider itself
+            // prevent sideways scrolling
+            e.preventDefault();
+            // reset position of all sliding divs to off-screen
+            resetSlidingDivs();
+            // show the sliding div corresponding to the swipe position
+            e.target.closest('.slider').querySelector('.slide-on-btn-container').style.transform = `translateX(calc(100vw - 15rem))`; // ensure we are targeting the slider itself
         } else {
             // swiped right
-            console.log("swiped right");
-            console.log(e.target);
             e.target.closest('.slider').querySelector('.slide-on-btn-container').style.transform = `translateX(100vw)`; // ensure we are targeting the slider itself
-
         }
     }
 
@@ -63,6 +65,59 @@ function moveTouch(e) {
     initialY = null;
 }
 
+function resetSlidingDivs() {
+    for (let i = 0; i < slidingBtnContainer.length; i++) {
+        slidingBtnContainer[i].style.transform = `translateX(100vw)`;
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////  ////
+//// SLIDE UP FORMS - EDIT SET AND ADD EXERCISE  ////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const footerAddExercise = document.querySelector(".footer__icon--add");
+const footerDeleteGroup = document.querySelector(".footer__icon--delete");
+const formAddExerciseToCurrent = document.querySelector(".add-to-current");
+const formEditSetInfo = document.querySelector(".edit-set");
+const addToCurrentClose = document.querySelector(".add-to-current--close");
+const editSetClose = document.querySelector(".edit-set--close");
+const editSetBtns = document.querySelectorAll('.slide-on-btn--edit');
+const editWeightInput = document.querySelector("#edit-weight");
+
+
+footerAddExercise.addEventListener('click', function() {
+    slideFormUp(formAddExerciseToCurrent, -42);
+})
+
+addToCurrentClose.addEventListener('click', function() {
+    slideFormDown(formAddExerciseToCurrent);
+})
+
+// TODO: Apply this to the body and use bubbling to target future edit btns.
+for (let i = 0; i < editSetBtns.length; i++) {
+    editSetBtns[i].addEventListener('click', function() {
+        slideFormUp(formEditSetInfo, -34);
+        //editWeightInput.select(); buggy on iphone - causes large area under footer to become visible
+    })
+}
+
+editSetClose.addEventListener('click', function() {
+    slideFormDown(formEditSetInfo);
+})
+
+
+function slideFormUp(formElement, slideDistanceREM) {
+    formElement.classList.remove('display-none');
+    setTimeout(function() {
+        formElement.classList.add('display-hidden');
+        formElement.classList.remove('display-hidden');
+        formElement.style.transform = `translateY(${slideDistanceREM}rem)`;
+    }, 0);
+}
+
+function slideFormDown(formElement) {
+    formElement.style.transform = 'translateY(0rem)';
+    setTimeout(function() {
+        formElement.classList.add('display-none');
+    }, 500);
+}
